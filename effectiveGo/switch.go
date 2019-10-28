d1fca0568de168b1a5f564a49b81be59af85f968
IMalyanova <malyanova.ira@gmail.com>
2019-10-28 13:15:25 +0300

    effectiveGo add

--- effectiveGo/switch.go
+++ effectiveGo/switch.go
@@ -43,8 +43,27 @@ Loop:
 			}
 			if validateOnly{
 				break
-			}				
+			}
 			size = 2
 			update (src[n] + src[n+1] << shift)
 		}
 	}
+	//=========================================
+
+func Compare(a, b []byte) int {
+	for i := 0; i < len(a) && i < len(b) ; i++  {
+		switch {
+		case a[i] > b[i]:
+			return  1
+		case a[i] < b[i]:
+			return  -1
+		}
+	}
+	switch {
+	case len(a) > len(b):
+		return 1
+	case len(a) > len(b):
+		return -1
+	}
+	return 0
+}//================================================
