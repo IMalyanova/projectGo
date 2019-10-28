20b67b0610bfa667d4d7077233dea2b7825cc320
IMalyanova <malyanova.ira@gmail.com>
2019-10-28 12:01:12 +0300

    effectiveGo add

---
+++ effectiveGo/for.go
@@ -0,0 +1,29 @@
+package main
+
+func main() {
+	//for
+	for init; condition; post{}
+
+	//while
+	for condition{}
+
+	//for ; ;
+	for{}
+	//================================
+	summ: = 0
+	for i: = 0; i < 10; i ++ {
+		sum + = i
+	}
+	//	============================
+	for key, value := range oldMap {
+		newMap[key] = value
+	}
+	//=================================
+	for key := range m {
+		if key.expired(){
+			delete(m, key)
+		}
+	}
+	//	=========================
+
+}
