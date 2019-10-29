b505ddc1b869840e60d0c9b5057d3bab17460b04
IMalyanova <malyanova.ira@gmail.com>
2019-10-29 17:11:43 +0300

    update errors

--- effectiveGo/errors.go
+++ effectiveGo/errors.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
 
 }
@@ -15,3 +20,58 @@ type PathError struct {
 	Path string
 	Err  error
 }
+
+func (e *PathError) Error() string {
+	return e.Op + " " + e.Path + ": " + e.Err.Error()
+}
+
+//=======
+
+open /etc/passwx: no such file or directory
+
+//=======
+
+for try := 0; try < 2; try++ {
+	file, err = os.Create(filename)
+
+	if err == nil {
+		return
+	}
+
+	if e, ok := errr.(*os.PathError); ok && e.Err == syscall.ENOSPC {
+		deleteTempFiles()
+		continue
+	}
+
+	return
+}
+
+//=======Panic
+
+func CubeRoot(x float64) float64  {
+	z := x/3
+
+	for i := 0; i < 1e6; i ++  {
+		prevz := z
+		z -= (z * z * z - x) / (3 * z * z)
+
+		if veryClose(z, prevz) {
+			return z
+		}
+	}
+
+	panic( fmt.Sprintf("CubeRoot(%g) did not converge", x))
+}
+
+//=======
+
+var user = os.Getenv("USER")
+
+func init() {
+
+	if usser == "" {
+		panic("No value for $USER")
+	}
+}
+
+//=======
