d98c49db0ae96d30b0481a3acbfec6ecfdd5028c
IMalyanova <malyanova.ira@gmail.com>
2019-10-29 16:08:06 +0300

    Add concurrents

--- effectiveGo/concurrency.go
+++ effectiveGo/concurrency.go
@@ -102,3 +102,36 @@ func Serve(clientRequests chan *Request, quit chan bool)  {
 
 //=======
 
+type Request struct {
+	args [] int
+	f func([] int) int
+	resultChan chan int
+}
+
+//=======
+
+func sum(a []int) (s int) {
+	for _, v := range a {
+		s += v
+	}
+	return
+}
+
+request := &Request {[]int{2, 4, 5}, sum, make(chan int)}
+clientRequests <- request
+fmt.Printf("answer: %d\n", <-request.resultChan)
+
+//=======
+
+func handle(queue chan *Request){
+	for req := range queue {
+		req.resultChan <- req.f(req.args)
+	}
+}
+
+//=======Parallelization
+
+
+
+
+
