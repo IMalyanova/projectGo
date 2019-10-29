da799e3e13f61ce97285775fef99717cdcf1da65
IMalyanova <malyanova.ira@gmail.com>
2019-10-29 10:40:59 +0300

    update effectiveGo

--- effectiveGo/initialization.go
+++ effectiveGo/initialization.go
@@ -1,11 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 type ByteSize float64
 
 const (
-	_=iota
+	_           = iota
 	KB ByteSize = 1 << (10 * iota)
 	MB
 	GB
@@ -17,20 +20,49 @@ const (
 )
 
 func main() {
-	
+
 }
 
-func (b ByteSize) String() string{
+func (b ByteSize) String() string {
 	switch {
 	case b >= YB:
-		return  fmt.Sprintf("%.2fYB", b/YB)
+		return fmt.Sprintf("%.2fYB", b/YB)
 	case b >= ZB:
 		return fmt.Sprintf("%.2ZB", b/ZB)
 	case b >= EB:
 		return fmt.Sprintf("%.2fEB", b/EB)
 	case b >= PB:
 		return fmt.Sprintf("%.2fPB", b/PB)
-
-
+	case b >= TB:
+		return fmt.Sprintf("%.2TB", b/TB)
+	case b >= GB:
+		return fmt.Sprintf("%.2GB", b/GB)
+	case b >= MB:
+		return fmt.Sprintf("%.2MB", b/MB)
+	case b >= KB:
+		return fmt.Sprintf("%.2KB", b/KB)
 	}
+	return fmt.Sprintf("%.2fB", b)
 }
+
+//==========================================
+type ByteSize float64
+
+const (
+	_           = iota
+	KB ByteSize = 1 << (10 * iota)
+	MB
+	GB
+	TB
+	PB
+	EB
+	ZB
+	YB
+)
+
+//===============================
+var (
+	home   = os.Getenv("HOME")
+	user   = os.Getenv("USER")
+	gopath = os.Getenv("GOPATH")
+)
