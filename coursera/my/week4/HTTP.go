4778e4b420fe0202f9029403fcd2e44955827af7
IMalyanova <malyanova.ira@gmail.com>
2019-11-07 10:27:55 +0300

    add week4 serverHTTP

---
+++ coursera/my/week4/HTTP.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request)  {
+
+	fmt.Fprintln(w, "Hello!")
+	w.Write([]byte("!!!"))
+}
+
+
+func main() {
+
+	http.HandleFunc("/", handler)
+
+	fmt.Println("starting server at :8080")
+	http.ListenAndServe(":8080", nil)
+}
