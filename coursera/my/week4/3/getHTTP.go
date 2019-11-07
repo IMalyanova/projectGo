06cb05b8952380d0c417d201172c876760690c99
IMalyanova <malyanova.ira@gmail.com>
2019-11-07 14:47:43 +0300

    update week4

--- coursera/my/week4/3/getHTTP.go
+++ coursera/my/week4/3/getHTTP.go
@@ -19,5 +19,15 @@ func main() {
 
 func handler(writer http.ResponseWriter, request *http.Request) {
 
-	myParam := request.URL.Query().
+	myParam := request.URL.Query().Get("param")
+
+	if myParam != "" {
+		fmt.Fprintln(writer, "`myParam` is", myParam)
+	}
+
+	key := request.FormValue("key")
+
+	if key != "" {
+		fmt.Fprintln(writer, "`key` is", key)
+	}
 }
