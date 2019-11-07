b1f343c904b4e5227a3078c44675a413e5afeae3
IMalyanova <malyanova.ira@gmail.com>
2019-11-07 18:15:21 +0300

    week4

---
+++ coursera/my/week4/6/uploadRawBody.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"coursera/my/week4/5"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	
+}
+
+
+
+func uploadRawBody(w http.ResponseWriter, r *http.Request) {
+
+	body, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	p := &_.Params{}
+	err = json.Unmarshal(body, p)
+
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	fmt.Fprintf(w, "content-type %#v\n",
+		r.Header.Get("Content-Type"))
+	fmt.Fprintf(w, "params %#v\n", p)
+}
+
+
