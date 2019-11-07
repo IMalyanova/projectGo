2b79ee7fedaefef9e9329f5dbf76526959d305f1
IMalyanova <malyanova.ira@gmail.com>
2019-11-07 10:35:01 +0300

    update serverHTTP

--- coursera/my/week4/serverHTTP.go
+++ coursera/my/week4/serverHTTP.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
-	
+
+	testHandler := &Handler{Name: "test"}
+	http.Handle("/test/", testHandler)
+
+	rootHandler := &Handler{Name: "root"}
+	http.Handle("/", rootHandler)
+
+	fmt.Println("starting server at :8080")
+	http.ListenAndServe(":8080", nil)
 }
