4ce8571b1c1a19b448c1e17c231a06a3f9c4ff20
IMalyanova <malyanova.ira@gmail.com>
2019-11-07 14:34:40 +0300

    update

--- coursera/my/week4/3/getHTTP.go
+++ coursera/my/week4/3/getHTTP.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+
 func main() {
-	
+
+	http.HandleFunc("/", handler)
+
+	fmt.Println("starting server at :8080")
+	http.ListenAndServe(" :8080", nil)
+}
+
+
+
+
+func handler(writer http.ResponseWriter, request *http.Request) {
+
+	myParam := request.URL.Query().
 }
