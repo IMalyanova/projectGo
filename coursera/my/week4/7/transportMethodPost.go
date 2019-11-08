349c3c6e78a6d5dd3a2de712f2766b965a3dc662
IMalyanova <malyanova.ira@gmail.com>
2019-11-08 13:01:47 +0300

    week4->7

--- coursera/my/week4/7/transportMethodPost.go
+++ coursera/my/week4/7/transportMethodPost.go
@@ -2,10 +2,14 @@ package main
 //мы отправляем запрос на сервер - Get, Post
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
+	"net"
 	"net/http"
 	"net/url"
+	"strconv"
+	"time"
 )
 
 
@@ -79,7 +83,56 @@ func runGetFullReq(){
 
 
 
+func runTransportAndPost() {
+
+	transport := &http.Transport {
+		DialContext: (&net.Dialer{
+			Timeout:    30 * time.Second,
+			KeepAlive:  30 * time.Second,
+			DualStack:  true,
+	}).DialContext,
+	MaxIdleConns:          100,
+	IdleConnTimeout:       90 * time.Second,
+	TLSHandshakeTimeout:   10 * time.Second,
+	ExpectContinueTimeout: 1  * time.Second,
+	}
+
+	client := &http.Client {
+		Timeout: time.Second * 10,
+		Transport: transport,
+	}
+
+	data := `{"id": 42, "user": "rvasily"}`
+	body := bytes.NewBufferString(data)
+
+	url := "http://127.0.0.1:8080/raw body"
+	req,_ := http.NewRequest(http.MethodPost, url, body)
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Content-Length", strconv.Itoa(len(data)))
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		fmt.Println("error happend", err)
+		return
+	}
+	defer resp.Body.Close()   // важный пункт
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+
+	fmt.Printf("runTransport %#v\n\n\n", string(respBody))
+}
+
+
+
 
 func main() {
-	
+
+	go startServer()
+
+	time.Sleep(100 * time.Millisecond)
+
+	runGet()
+	runGetFullReq()
+	runTransportAndPost()
 }
