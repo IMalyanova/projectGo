fb55ea3d16965604ab7f2ee00a10bbe105db81d8
IMalyanova <malyanova.ira@gmail.com>
2019-11-11 12:18:41 +0300

    update

--- coursera/my/part2/middleware.go
+++ coursera/my/part2/middleware.go
@@ -57,3 +57,92 @@ func pageWithAllChecks(w http.ResponseWriter, r *http.Request) {
 
 	// Your logic
 }
+
+
+func panicMiddleware(next http.Handler) http.Handler {
+
+	return http.HandleFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		fmt.Println("panicMiddleware", r.URL.Path)
+
+		defer func() {
+			if err := recover(); err != nil {
+				fmt.Println("recovered", err)
+				http.Error(w, "Internal server error", 500)
+			}
+		}()
+		next.ServeHTTP(w, r)
+	})
+}
+
+
+func accessLogMiddleware(next http.Handler) http.Handler {
+
+	return http.HandleFunc(func (w http.ResponseWriter, r *http.Request) {
+
+		fmt.Println("accessLogMiddleware", r.URL.Path)
+		start := time.Now()
+		next.ServeHTTP(w, r)
+		fmt.Printf("[%s] %s, %s %s\n",
+			r.Method, r.RemoteAddr, r.URL.Path, time.Since(start))
+	})
+}
+
+
+func panicMiddleware(next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		fmt.Println("panicMiddleware", r.URL.Path)
+		defer func() {
+			if err := recover(); err != nil {
+				fmt.Println("recovered", err)
+				http.Error(w, "Internal server error", 500)
+				return
+			}
+			next.ServeHTTP(w, r)
+		}
+	})
+}
+
+//==============
+
+func adminIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, `<a href="/">site index</a>`)
+	fmt.Fprintln(w, "Admin main page")
+}
+
+
+func panicPage(w http.ResponseWriter, r *http.Request) {
+	panic("this must me recovered")
+}
+
+//==============
+
+func loginPage(w http.ResponseWriter, r *http.Request) {
+
+	expiration := time.Now().Add(10 * time.Hour)
+	cookie := http.Cookie {
+		Name: "session_id",
+		Value: "rvasily",
+		Expires: expiration,
+	}
+	http.SetCookie(w, &cookie)
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+
+func logoutPage(w http.ResponseWriter, r *http.Request) {
+
+	session, err := r.Cookie("session_id")
+
+	if err == http.ErrNoCookie {
+		http.Redirect(w, r, "/", http.StatusFound)
+		return
+	}
+	session.Expires = time.Now().AddDate(0, 0, -1)
+	http.SetCookie(w, session)
+	http.Redirect(w, r, "/", http.StatusFound)
+	return
+}
+
