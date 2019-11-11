8117bd7960025fcd2ef2b4cdd0a7a69adde2f281
IMalyanova <malyanova.ira@gmail.com>
2019-11-11 12:52:47 +0300

    update

--- coursera/my/part2/middleware.go
+++ coursera/my/part2/middleware.go
@@ -6,31 +6,6 @@ import (
 	"time"
 )
 
-func main() {
-
-	adminMux := http.NewServeMux()
-	adminMux.HandleFunc("/admin/", adminIndex)
-	adminMux.HandleFunc("/admin/panic", panicPage)
-
-	// set middleware
-	adminHandler := adminAuthMiddleware(adminMux)
-
-	siteMux := http.NewServeMux()
-	siteMux.Handle("/admin/", adminHandler)
-	siteMux.HandleFunc("/login", loginPage)
-	siteMux.HandleFunc("/logout", logoutPage)
-	siteMux.HandleFunc("/", mainPage)
-
-	// set middleware
-	siteHandler := accessLogMiddleware(siteMux)
-	siteHandler = panicMiddleware(siteHandler)
-
-	fmt.Println("starting server at :8080")
-	http.ListenAndServe(" :8080", siteHandler)
-}
-
-
-
 func pageWithAllChecks(w http.ResponseWriter, r *http.Request) {
 
 	defer func() {
@@ -38,13 +13,10 @@ func pageWithAllChecks(w http.ResponseWriter, r *http.Request) {
 			fmt.Println("recovered", err)
 		}
 	}()
-
-
 	defer func(start time.Time) {
 		fmt.Printf("[%s] %s, %s %s\n",
 			r.Method, r.RemoteAddr, r.URL.Path, time.Since(start))
 	}(time.Now())
-
 
 	_, err := r.Cookie("session_id")
 	// Учебный пример! Это не проверка авторизации!
@@ -59,27 +31,8 @@ func pageWithAllChecks(w http.ResponseWriter, r *http.Request) {
 }
 
 
-func panicMiddleware(next http.Handler) http.Handler {
-
-	return http.HandleFunc(func(w http.ResponseWriter, r *http.Request) {
-
-		fmt.Println("panicMiddleware", r.URL.Path)
-
-		defer func() {
-			if err := recover(); err != nil {
-				fmt.Println("recovered", err)
-				http.Error(w, "Internal server error", 500)
-			}
-		}()
-		next.ServeHTTP(w, r)
-	})
-}
-
-
 func accessLogMiddleware(next http.Handler) http.Handler {
-
-	return http.HandleFunc(func (w http.ResponseWriter, r *http.Request) {
-
+	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
 		fmt.Println("accessLogMiddleware", r.URL.Path)
 		start := time.Now()
 		next.ServeHTTP(w, r)
@@ -100,8 +53,8 @@ func panicMiddleware(next http.Handler) http.Handler {
 				http.Error(w, "Internal server error", 500)
 				return
 			}
-			next.ServeHTTP(w, r)
-		}
+		}()
+		next.ServeHTTP(w, r)
 	})
 }
 
@@ -145,4 +98,67 @@ func logoutPage(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/", http.StatusFound)
 	return
 }
+
+
+func mainPage(w http.ResponseWriter, r *http.Request) {
+
+	session, err := r.Cookie("session_id")
+
+	// учебный пример! это не проверка авторизации!
+
+	loggedIn := (err != http.ErrNoCookie)
+
+	if loggedIn {
+		fmt.Fprintln(w, `<a href="/logout">logout</a>`)
+		fmt.Fprintln(w, "Welcome, " + session.Value)
+	} else {
+		fmt.Fprintln(w, `<a href="/login">login</a>`)
+		fmt.Fprintln(w, "You need to login")
+	}
+}
+
+
+func adminAuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
+
+		fmt.Println("adminAuthMiddleware", r.URL.Path)
+		_, err := r.Cookie("session_id")
+		// учебный пример! это не проверка авторизации!
+
+		if err != nil {
+			fmt.Println("no auth at ", r.URL.Path)
+			http.Redirect(w, r, "/", http.StatusFound)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+//----------------
+
+
+func main() {
+
+	adminMux := http.NewServeMux()
+	adminMux.HandleFunc("/admin/", adminIndex)
+	adminMux.HandleFunc("/admin/panic", panicPage)
+
+	// set middleware
+	adminHandler := adminAuthMiddleware(adminMux)
+
+	siteMux := http.NewServeMux()
+	siteMux.Handle("/admin/", adminHandler)
+	siteMux.HandleFunc("/login", loginPage)
+	siteMux.HandleFunc("/logout", logoutPage)
+	siteMux.HandleFunc("/", mainPage)
+
+	// set middleware
+	siteHandler := accessLogMiddleware(siteMux)
+	siteHandler = panicMiddleware(siteHandler)
+
+	fmt.Println("starting server at :8080")
+	http.ListenAndServe(" :8080", siteHandler)
+}
+
+
 
