c7795948e8e19b147be3ec59daf625dd6977792f
IMalyanova <malyanova.ira@gmail.com>
2019-11-12 11:30:38 +0300

    add multiple

---
+++ coursera/my/part2/multiple.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	fastApiHandler := httprouter.New()
+	fastApiHandler.GET("/fast/:id", FastRequest)
+
+	complexApiHandler := mux.NewRouter()
+	complexApiHandler.HandleFunc("/complex/", ComplexRequest).Headers("X-Requested-With",
+		"XMLHttpRequest")  // ajax
+
+	stdApiHandler := http.NewServeMux()
+	stdApiHandler.HandleFunc("/std/", RegularRequest)
+
+	siteMux := http.NewServeMux()
+	siteMux.Handle("/fast/", fastApiHandler)
+	siteMux.Handle("/complex/", complexApiHandler)
+	siteMux.Handle("/std/", stdApiHandler)
+	
+	fmt.Println("starting server at :8080")
+	log.Fatal(http.ListenAndServe(":8080", siteMux))
+}
+
+
+
+func RegularRequest(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Request with average amount of logic\n")
+}
+
+
+
+func FastRequest(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	fmt.Fprint(w, "Request with high hitrate\n")
+}
+
+
+
+func ComplexRequest(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Request with complex routing logic\n")
+}
